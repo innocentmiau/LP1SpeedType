c546b50264b9ebcdb60f40ae155ea0002b8e2119
André <imandreleandro@gmail.com>
2025-04-11 14:08:29 +0100

    Worked all good, nice

--- SpeedType/Game.cs
+++ SpeedType/Game.cs
@@ -119,6 +119,7 @@ namespace SpeedType
             for (int i = gameStats.Length - 1; i > 0; i--)
             {
                 // ////////// => TO IMPLEMENT <= //////////// //
+                gameStats[i] = gameStats[i - 1];
             }
 
             // Add new result at the beginning
